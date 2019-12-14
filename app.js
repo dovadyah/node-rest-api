0dcc70bb6cfcd8d5426e8bfb2642906511303cf8
Daniel Ovadyah <dovadyah@gmail.com>
2019-12-14 07:30:26 -0500

    some refactoring

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var corsHandler = require('./api/handlers/cors-handler');
 var app = express();
 
 //db params
-var uri = process.env.MongoDBAtlasURIs;
+var uri = process.env.MongoDBAtlasURI;
 var dbOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true
